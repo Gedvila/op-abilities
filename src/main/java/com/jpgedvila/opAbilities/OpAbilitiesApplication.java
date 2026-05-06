5eca80a9e46307655afcaa1bc6ae9b660b07237c
YamiGedvila <jpgedvila0306@gmail.com>
2026-05-06 14:47:22 -0300

    First commit

---
+++ src/main/java/com/jpgedvila/opAbilities/OpAbilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.jpgedvila.opAbilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpAbilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpAbilitiesApplication.class, args);
+	}
+
+}
