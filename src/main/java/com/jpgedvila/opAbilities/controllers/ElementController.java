f221ec7c67df9d3c00b2a094e38779cca0e3f357
YamiGedvila <jpgedvila0306@gmail.com>
2026-05-22 23:09:35 -0300

    ExceptionHandler implementation | Element post,put and delete

--- src/main/java/com/jpgedvila/opAbilities/controllers/ElementController.java
+++ src/main/java/com/jpgedvila/opAbilities/controllers/ElementController.java
@@ -3,11 +3,10 @@ package com.jpgedvila.opAbilities.controllers;
 import com.jpgedvila.opAbilities.dtos.ElementDTO;
 import com.jpgedvila.opAbilities.services.ElementService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,21 +15,47 @@ import java.util.List;
 public class ElementController {
 
     private final ElementService service;
-    public ElementController(ElementService service){
-        this.service =service;
+
+    public ElementController(ElementService service) {
+        this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<ElementDTO>> findElement(@RequestParam(name = "id", defaultValue = "0") Long id){
+    public ResponseEntity<List<ElementDTO>> findElement(@RequestParam(name = "id", defaultValue = "0") Long id) {
 
         List<ElementDTO> result = new ArrayList<>();
 
-        if (id == 0){
-              result = service.findAll();
+        if (id == 0) {
+            result = service.findAll();
         } else {
             result.add(service.findById(id));
         }
 
         return ResponseEntity.ok(result);
+    }
+
+    @PutMapping
+    public ResponseEntity<ElementDTO> updateElement(@RequestParam(name = "id") Long id, @RequestBody ElementDTO dto) {
+
+        dto = service.update(id, dto);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ElementDTO> insertElement(@RequestBody ElementDTO dto) {
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("?id={id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteElement(@RequestParam(name = "id") Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/jpgedvila/opAbilities/services/ElementService.java
+++ src/main/java/com/jpgedvila/opAbilities/services/ElementService.java
@@ -3,7 +3,11 @@ package com.jpgedvila.opAbilities.services;
 import com.jpgedvila.opAbilities.dtos.ElementDTO;
 import com.jpgedvila.opAbilities.entities.Element;
 import com.jpgedvila.opAbilities.repositories.ElementRepository;
+import com.jpgedvila.opAbilities.services.exceptions.DatabaseException;
+import com.jpgedvila.opAbilities.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,34 +16,81 @@ import java.util.List;
 public class ElementService {
 
     private final ElementRepository repository;
-    public ElementService(ElementRepository elementRepository){
+
+    public ElementService(ElementRepository elementRepository) {
         this.repository = elementRepository;
     }
 
     @Transactional(readOnly = true)
-    public List<ElementDTO> findAll(){
+    public List<ElementDTO> findAll() {
         List<Element> elements = repository.findAll();
 
-        return elements.stream().map(ElementDTO :: new).toList();
+        return elements.stream().map(ElementDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
-    public ElementDTO findById(Long id){
+    public ElementDTO findById(Long id) {
 
-        Element element = repository.findById(id).orElseThrow();
+        Element element = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado")
+        );
 
         return new ElementDTO(element);
     }
 
     @Transactional(readOnly = true)
-    public ElementDTO searchByName(String name){
+    public ElementDTO searchByName(String name) {
 
         try {
             Element element = repository.searchByName(name);
 
             return new ElementDTO(element);
-        } catch (Exception e){
-            throw new RuntimeException();
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
         }
+    }
+
+    @Transactional
+    public ElementDTO insert(ElementDTO dto) {
+        Element entity = new Element();
+        copyDtoToEntity(entity, dto);
+
+        entity = repository.save(entity);
+
+        return new ElementDTO(entity);
+    }
+
+    @Transactional
+    public ElementDTO update(Long id, ElementDTO dto) {
+
+        try {
+
+            Element entity = repository.getReferenceById(id);
+            copyDtoToEntity(entity, dto);
+
+            entity = repository.save(entity);
+
+            return new ElementDTO(entity);
+        } catch (RuntimeException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(Element entity, ElementDTO dto) {
+        entity.setName(dto.getName());
     }
 }
