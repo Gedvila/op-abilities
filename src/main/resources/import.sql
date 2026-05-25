74eb730d5dd9d87815244e92f5ce6f22c6482934
YamiGedvila <jpgedvila0306@gmail.com>
2026-05-25 17:52:30 -0300

    CorsConfig

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,9 @@ INSERT INTO tb_elements(name) VALUES ('Medo');
 INSERT INTO tb_items_prop(name,description) VALUES ('Armas Ágeis','Facas, punhais, cajados, nunchakus, floretes e katanas permitem que você aplique sua Agilidade em vez de sua Força em testes de ataque e rolagens de dano realizadas com elas.');
 INSERT INTO tb_items_prop(name,description) VALUES ('Armas Automáticas','Fuzis de assalto,submetralhadoras e metralhadoras podem disparar tiro únicos, usando a regra normal, ou rajadas. Quando dispara uma rajada você sofre -1d20 no teste de ataque, mas causa 1 dado de dano adicional do mesmo tipo');
 INSERT INTO tb_items_prop(name,description) VALUES ('Coronhada','Você pode usar uma arma de fogo como uma arma corpo a corpo. O dano é 1d4 de impacto para armas de fogo leves e de uma mão (como pistolas) e 1d6 para armas de fogo de duas mãos (como fuzis).');
+
+INSERT INTO tb_damage_kind(name, description) VALUES ('Balístico','Dano causado por projéteis arremessados a altas velocidades, como balas de um fuzil. Armas de fogo são as principais fontes deste tipo de dano.');
+INSERT INTO tb_damage_kind(name, description) VALUES ('Corte','Armas afiadas, como espadas, machados e as garras de um animal, causam dano de corte');
+INSERT INTO tb_damage_kind(name, description) VALUES ('Eletricidade','Alguns rituais, itens e perigos naturais (como um relâmpago) causam dano deste tipo.');
+INSERT INTO tb_damage_kind(name, description) VALUES ('Fogo','Causado por calor e chamas, tanto naturais quanto conjuradas por rituais.');
+INSERT INTO tb_damage_kind(name, description) VALUES ('Frio','Alguns rituais, além de clima severo, causam dano de frio.');
