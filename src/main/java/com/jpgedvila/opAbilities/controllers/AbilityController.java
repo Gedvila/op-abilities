3c59141c0701a5c172a1f13d0abfdeb83e2fee3c
YamiGedvila <jpgedvila0306@gmail.com>
2026-05-29 12:34:25 -0300

    AbilityController

---
+++ src/main/java/com/jpgedvila/opAbilities/controllers/AbilityController.java
@@ -0,0 +1,9 @@
+package com.jpgedvila.opAbilities.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/ab")
+public class AbilityController {
+}
